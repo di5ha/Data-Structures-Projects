3d73b17c05222ef428029e22acfcd2eadc2d5a05
Disha <108921153+di5ha@users.noreply.github.com>
2023-08-15 09:57:24 +0530

    Add files via upload

---
+++ Project-1/Binary search tree/bst_ops.py
@@ -0,0 +1,95 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, root, key):
+        if root is None:
+            return TreeNode(key)
+
+        if key < root.key:
+            root.left = self._insert_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._insert_recursive(root.right, key)
+
+        return root
+
+    def search(self, key):
+        return self._search_recursive(self.root, key)
+
+    def _search_recursive(self, root, key):
+        if root is None or root.key == key:
+            return root
+
+        if key < root.key:
+            return self._search_recursive(root.left, key)
+        else:
+            return self._search_recursive(root.right, key)
+
+    def delete(self, key):
+        self.root = self._delete_recursive(self.root, key)
+
+    def _delete_recursive(self, root, key):
+        if root is None:
+            return root
+
+        if key < root.key:
+            root.left = self._delete_recursive(root.left, key)
+        elif key > root.key:
+            root.right = self._delete_recursive(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            root.key = self._min_value_node(root.right).key
+            root.right = self._delete_recursive(root.right, root.key)
+
+        return root
+
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+# Example usage
+bst = BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(70)
+bst.insert(20)
+bst.insert(40)
+bst.insert(60)
+bst.insert(80)
+
+print("Inorder traversal of the BST:")
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.key, end=" ")
+        inorder_traversal(root.right)
+
+inorder_traversal(bst.root)
+print()
+
+# Search for a key
+key_to_search = 40
+if bst.search(key_to_search):
+    print(f"Key {key_to_search} found.")
+else:
+    print(f"Key {key_to_search} not found.")
+
+# Delete a key
+key_to_delete = 30
+bst.delete(key_to_delete)
+print("Inorder traversal after deleting", key_to_delete)
+inorder_traversal(bst.root)
